53f7b7c3930ef72f6e23f47d26a6f8e5b0624339
Erhahon Imafidon <mafiosco4@gmail.com>
2024-09-26 15:45:20 +0100

    congigured the fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        workSans: ['Work Sans', 'sans-serif'],
+      },
+      color: {
+        white: 'hsl(0, 0%, 100%)',
+        'light-pink': 'hsl(275, 100%, 97%)',
+        'gray-purple': 'hsl(292, 16%, 49%)',
+        'dark-purple': 'hsl(292, 42%, 14%)'
+      }
+    },
   },
   plugins: [],
 }
